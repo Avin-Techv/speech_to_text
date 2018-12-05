1c407995d2decd961e35779a5022092fa1a3e3e9
Avin-Techv <avin.mathew@techversantinc.com>
2018-12-05 18:50:28 +0530

    homepage form interface added

--- package_select/forms.py
+++ package_select/forms.py
@@ -3,8 +3,7 @@ from .models import Sound
 
 
 class SelectPackageForm(forms.ModelForm):
-    file_path = forms.CharField(max_length=50)
 
     class Meta:
             model = Sound
-            fields = ['description', 'sound']
+            fields = ['input_method', 'document', 'recognition_package']
--- package_select/models.py
+++ package_select/models.py
@@ -2,8 +2,23 @@ from django.db import models
 
 # Create your models here.
 
+INPUT_METHOD = (
+    ('upload', 'UPLOAD FILE'),
+    ('record', 'RECORD'),
+)
+
+RECOGNITION_PACKAGE = (
+    ('bing', 'Microsoft Bing Speech'),
+    ('google', 'Google Web Speech API'),
+    ('google_cloud', 'Google Cloud Speech'),
+    ('houndify', 'Houndify'),
+    ('ibm', 'IBM Speech to Text'),
+    ('sphinx', ' CMU Sphinx'),
+    ('wit', 'Wit.ai'),
+)
+
 
 class Sound(models.Model):
-    description = models.CharField(max_length=255, blank=True)
-    sound = models.FileField(upload_to='sounds/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    input_method = models.CharField(choices=INPUT_METHOD, default='upload', max_length=255)
+    document = models.FileField(upload_to='documents/')
+    recognition_package = models.CharField(choices=RECOGNITION_PACKAGE, default='upload', max_length=255)
