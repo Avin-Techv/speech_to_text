0c4df6a44cadf80870f7754321e1df08dd44bd89
Avin-Techv <avin.mathew@techversantinc.com>
2018-12-11 16:50:15 +0530

    uploaded files list done

--- package_select/urls.py
+++ package_select/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SelectPackage, UploadFile, RecordFile
+from .views import SelectPackage, UploadFile, RecordFile, FilesList
 app_name = 'package_select'
 
 urlpatterns = [
     path('', SelectPackage.as_view(), name="home"),
     path('upload/', UploadFile.as_view(), name="upload"),
-    path('record/', RecordFile.as_view(), name="record")
+    path('record/', RecordFile.as_view(), name="record"),
+    path('files/', FilesList.as_view(), name="files")
 ]
--- package_select/views.py
+++ package_select/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.views.generic import FormView
+from django.views.generic import FormView, TemplateView
 from .forms import SelectPackageForm, UploadFileForm, RecordFileForm
+from .models import Document
+
+from django.views.generic import View
+from django.urls import reverse_lazy
 
 
 class SelectPackage(FormView):
@@ -15,8 +19,31 @@ class UploadFile(FormView):
     form_class = UploadFileForm
     success_url = '.'
 
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(self.success_url)
+        else:
+            return render(request, self.template_name, {'form': form})
+
 
 class RecordFile(FormView):
     template_name = "package_select/record_file.html"
     form_class = RecordFileForm
     success_url = '.'
+
+
+class FilesList(TemplateView):
+    template_name = "package_select/files.html"
+
+    def get(self, request, *args, **kwargs):
+        documents = Document.objects.all()
+        return render(request, 'package_select/files.html', {'documents': documents})
+
+
+
