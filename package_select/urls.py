2372524d621e8ba68699b98a3a027d608d524a1b
Avin-Techv <avin.mathew@techversantinc.com>
2018-12-26 19:19:57 +0530

    new page to record and transcribe audio via web

--- package_select/urls.py
+++ package_select/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import SelectPackage, UploadFile, RecordFile, FilesList, AnalyseFile
+from .views import SelectPackage, UploadFile, RecordFile, FilesList, AnalyseFile, RecordPackageFile
 app_name = 'package_select'
 
 urlpatterns = [
     path('', SelectPackage.as_view(), name="home"),
     path('upload/', UploadFile.as_view(), name="upload"),
     path('record/', RecordFile.as_view(), name="record"),
+    path('record_package/', RecordPackageFile.as_view(), name="record_package"),
     path('files/', FilesList.as_view(), name="files"),
     path('analyse/', AnalyseFile.as_view(), name="analyse")
 ]
--- package_select/views.py
+++ package_select/views.py
@@ -69,3 +69,20 @@ class AnalyseFile(View):
             return render(request, 'package_select/files.html', {'content': content})
         except IOError:
             pass
+
+
+class RecordPackageFile(TemplateView):
+    template_name = "package_select/record_file_package.html"
+
+    def post(self, request, *args, **kwargs):
+        try:
+            r = sr.Recognizer()
+            with sr.Microphone() as source:
+                print("Say something!")
+                audio = r.listen(source)
+            print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
