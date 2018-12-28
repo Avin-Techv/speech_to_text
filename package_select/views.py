7e93eee6e06cd774a28690d250e1ca670e0720eb
Avin-Techv <avin.mathew@techversantinc.com>
2018-12-28 18:44:56 +0530

    implemented speech to text using django for a recording in timelimit

--- package_select/views.py
+++ package_select/views.py
@@ -1,4 +1,7 @@
 import os
+import time
+import schedule
+
 
 from django.conf import settings
 from django.shortcuts import render, redirect
@@ -8,10 +11,16 @@ from .forms import SelectPackageForm, UploadFileForm, RecordFileForm
 from .models import Document
 
 from django.http import HttpResponse
-import speech_recognition as sr
 from pydub import AudioSegment
 
 from os import path
+
+import speech_recognition as sr
+from gtts import gTTS
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+from pygame import mixer
+mixer.init()
 
 
 class SelectPackage(FormView):
@@ -74,15 +83,26 @@ class AnalyseFile(View):
 class RecordPackageFile(TemplateView):
     template_name = "package_select/record_file_package.html"
 
-    def post(self, request, *args, **kwargs):
-        try:
-            r = sr.Recognizer()
-            with sr.Microphone() as source:
-                print("Say something!")
-                audio = r.listen(source)
-            print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    def get(self, request, *args, **kwargs):
 
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            # print("Please wait. Calibrating microphone...")
+            # listen for 1 second and create the ambient noise energy level
+            r.adjust_for_ambient_noise(source, duration=1)
+            print("Say something!")
+            audio = r.listen(source, phrase_time_limit=15)
+            print(audio)
+
+        try:
+            response = r.recognize_google(audio)
+            print("I think you said '" + response + "'")
+            tts = gTTS(text="I think you said " + str(response), lang='en')
+            tts.save("response.mp3")
+            mixer.music.load('response.mp3')
+            mixer.music.play()
+            return render(request, 'package_select/record_file_package.html', {'response': response})
+        except sr.UnknownValueError:
+            error = "Google Speech Recognition could not understand audio"
+            return render(request, 'package_select/record_file_package.html', {'error': error})
